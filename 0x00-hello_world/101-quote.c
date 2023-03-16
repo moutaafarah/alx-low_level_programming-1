f27392445160742714d1bff75d4bd58e5370ae12
NPCxKali <lokmen.556@live.com>
2023-03-16 22:11:13 +0100

    Intel>AT&Tx5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *   
+ *   prints a String
+ *   Return: Always 1 (Success)
+**/
+
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
---
+++ 0x00-hello_world/6-size.s
@@ -0,0 +1,60 @@
+	.file	"6-size.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Size of a char: %zu byte(s)\n"
+.LC1:
+	.string	"Size of an int: %zu byte(s)\n"
+	.align 8
+.LC2:
+	.string	"Size of a long int: %zu byte(s)\n"
+	.align 8
+.LC3:
+	.string	"Size of a long long int: %zu byte(s)\n"
+.LC4:
+	.string	"Size of a float: %zu byte(s)\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$1, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$4, %esi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$8, %esi
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$8, %esi
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$4, %esi
+	leaq	.LC4(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 12.2.0-14) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
