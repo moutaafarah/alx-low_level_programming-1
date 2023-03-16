342638a60450ccd4b132861f06c0a7427de04092
NPCxKali <lokmen.556@live.com>
2023-03-16 20:57:59 +0100

    JEWEK A7LA JAW

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints a String
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+return (0);
+}
+
