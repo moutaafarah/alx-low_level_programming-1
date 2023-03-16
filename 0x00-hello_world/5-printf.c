272b46586ded44ce32cf13155c6ab8a346b3a4fb
NPCxKali <lokmen.556@live.com>
2023-03-16 19:24:29 +0100

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints a String
+ * Return: Always 0 (Success)
+ **/
+
+int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art,";
+return(0);
+}
+
